3b0d836666cfb6759087defc104a8034d64e55ea
Francisco Ruiz <fruiz.rc@gmail.com>
2022-02-15 15:31:39 -0400

    fix form for clients, add button for add new client in clients table and redirect when a new client is created

--- src/containers/Clients/index.jsx
+++ src/containers/Clients/index.jsx
@@ -1,13 +1,32 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 import { Section, ClientsTable } from '../../components';
 import { AppContext } from '../../context';
 
 function Clients() {
   const { clients, loading } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleAdd = () => {
+    navigate('/clients/create');
+  };
 
   return (
     <Section>
-      {!loading && <ClientsTable clients={clients.results} editable />}
+      {!loading && (
+        <>
+          <Button
+            size="large"
+            variant="outlined"
+            color="success"
+            onClick={handleAdd}
+          >
+            Añadir Cliente
+          </Button>
+          <ClientsTable clients={clients.results} editable />
+        </>
+      )}
     </Section>
   );
 }
--- src/containers/FormClient/index.jsx
+++ src/containers/FormClient/index.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Form } from '../../components';
 import { createClient } from '../../utils/api';
 import { AppContext } from '../../context';
 
 function FormClient() {
   const { token } = useContext(AppContext);
+  const navigate = useNavigate();
+
   const handleSubmit = async () => {
     const form = document.getElementById('form');
     const formData = new FormData(form);
     const { data, request } = await createClient({ formData, token });
+
+    if (request.ok) {
+      navigate('/clients');
+    }
   };
   return (
     <Form
